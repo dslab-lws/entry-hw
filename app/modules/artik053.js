1b3c07339c5c99c400a60605baaf782ffbf2312f
Nollirom <47169941+Nollirom@users.noreply.github.com>
2019-03-06 14:21:57 +0900

    Update artik053.js

--- app/modules/artik053.js
+++ app/modules/artik053.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 const BaseModule = require('./baseModule');
 
-class artik053 extends BaseModule {
+class ev3 extends BaseModule {
     constructor() {
         super();
         this.counter = 0;
@@ -10,35 +10,101 @@ class artik053 extends BaseModule {
         this.isSendInitData = false;
         this.isSensorCheck = false;
         this.isConnect = false;
-        this.digitalValue = new Array(14);
-        this.analogValue = new Array(6);
 
-        this.remoteDigitalValue = new Array(14);
-        this.readablePorts = null;
-        this.remainValue = null;
         this.sp = null;
         this.returnData = {};
 
     }
 
+    /**
+     * Direct Send Command 의 앞 부분을 설정한다.
+     *
+     * @returns {Buffer} size(2byte) + counter(2byte) + mode(1byte) + header(2byte)
+     * @param replyModeByte 0x00(reply required), 0x80(no reply)
+     * @param allocHeaderByte 할당된 결과값 byte 수를 나타낸다. 이 값이 4인 경우, 4byte 를 result value 로 사용한다.
+     */
+    makeInitBuffer(replyModeByte, allocHeaderByte) {
+        const size = new Buffer([0xff, 0xff]); // dummy 에 가깝다. #checkByteSize 에서 갱신된다.
+        const counter = this.getCounter();
+        const reply = new Buffer(replyModeByte);
+        const header = new Buffer(allocHeaderByte);
+        return Buffer.concat([size, counter, reply, header]);
+    }
 
+    /**
+     * 카운터를 가져온다. 카운터 값은 request & response 가 동일하여, 정상값 체크를 위해 사용된다.
+     * 이 값은 2^15 이상인 경우 0으로 초기화한다.
+     * @returns {Buffer} little endian 2byte
+     */
+    getCounter() {
+        let counterBuf = new Buffer(2);
+        counterBuf.writeInt16LE(this.counter);
+        if (this.counter >= 32767) {
+            this.counter = 0;
+        }
+        this.counter++;
+        return counterBuf;
+    }
+
+    /**
+     * size 를 해당하는 2byte 를 제외한 값을 size 에 씌운다.
+     *
+     * TODO 그렇다면 makeInitBuffer의 size는 영원히 아무일도 하지 않는다.
+     * @param buffer 파라미터가 완성된 buffer
+     */
+    checkByteSize(buffer) {
+        const bufferLength = buffer.length - 2;
+        buffer[0] = bufferLength;
+        buffer[1] = bufferLength >> 8; // buffer length 가 2^8 을 넘는 값일경우, 남은 값을 다음 size byte 에 씌운다.
+    }
+
+    /**
+     * 센서를 200ms 간격으로 체크한다. 센싱중에는 체크하지 않는다.
+     */
+    sensorChecking() {
+        if (!this.isSensorCheck) {
+            this.sensing = setInterval(() => {
+                this.sensorCheck();
+                this.isSensing = false;
+            }, 200);
+            this.isSensorCheck = true;
+        }
+    }
 
     init(handler, config) { }
 
     lostController() { }
 
+    eventController(state) {
+        if (state === 'connected') {
+            clearInterval(this.sensing);
+        }
+    }
 
     setSerialPort(sp) {
         this.sp = sp;
     }
 
-
+    /**
+     * 모터를 정지하고, output 센서를 체크한다.
+     * @param sp serial port
+     * @returns {null} 직접 serial port 에 ByteArray 를 작성한다.
+     */
     requestInitialData(sp) {
         this.isConnect = true;
         if (!this.sp) {
             this.sp = sp;
         }
 
+        if (!this.isSendInitData) {
+            const initBuf = this.makeInitBuffer([0x80], [0, 0]);
+            const motorStop = new Buffer([0xa3, 0x81, 0, 0x81, 0x0f, 0x81, 0]);
+            const initMotor = Buffer.concat([initBuf, motorStop]);
+            this.checkByteSize(initMotor);
+            sp.write(initMotor, () => {
+                this.sensorChecking();
+            });
+        }
         return null;
     }
 
@@ -48,89 +114,331 @@ class artik053 extends BaseModule {
 
     handleLocalData(data) {
         // data: Native Buffer
-        var pointer = 0;
-        for (var i = 0; i < 32; i++) {
-            var chunk;
-            if (!this.remainValue) {
-                chunk = data[i];
-            } else {
-                chunk = this.remainValue;
-                i--;
-            }
-            if (chunk >> 7) {
-                if ((chunk >> 6) & 1) {
-                    var nextChunk = data[i + 1];
-                    if (!nextChunk && nextChunk !== 0) {
-                        this.remainValue = chunk;
-                    } else {
-                        this.remainValue = null;
+        if (data[0] === this.wholeResponseSize + 3 && data[1] === 0) {
+            const countKey = data.readInt16LE(2);
+            if (countKey in this.SENSOR_COUNTER_LIST) {
+                this.isSensing = false;
+                delete this.SENSOR_COUNTER_LIST[countKey];
+                data = data.slice(5); // 앞의 4 byte 는 size, counter 에 해당한다. 이 값은 할당 후 삭제한다.
+                let index = 0;
 
-                        var port = (chunk >> 3) & 7;
-                        this.analogValue[port] =
-                            ((chunk & 7) << 7) + (nextChunk & 127);
+                Object.keys(this.SENSOR_MAP).forEach((p) => {
+                    const port = Number(p) - 1;
+                    index = port * this.commandResponseSize;
+
+                    const type = data[index];
+                    const mode = data[index + 1];
+                    let siValue = Number(
+                        (data.readFloatLE(index + 2) || 0).toFixed(1)
+                    );
+                    this.returnData[p] = {
+                        type: type,
+                        mode: mode,
+                        siValue: siValue,
+                    };
+                });
+
+                index = 4 * this.commandResponseSize;
+                Object.keys(this.BUTTON_MAP).forEach((button) => {
+                    if (data[index] === 1) {
+                        console.log(button + ' button is pressed');
                     }
-                    i++;
-                } else {
-                    var port = (chunk >> 2) & 15;
-                    this.digitalValue[port] = chunk & 1;
-                }
+
+                    this.returnData[button] = {
+                        pressed: data[index++] === 1,
+                    };
+                });
             }
         }
     }
 
     // Web Socket(엔트리)에 전달할 데이터
     requestRemoteData(handler) {
-        for (var i = 0; i < this.analogValue.length; i++) {
-            var value = this.analogValue[i];
-            handler.write('a' + i, value);
-        }
-        for (var i = 0; i < this.digitalValue.length; i++) {
-            var value = this.digitalValue[i];
-            handler.write(i, value);
-        }
+        Object.keys(this.returnData).forEach((key) => {
+            if (this.returnData[key] !== undefined) {
+                handler.write(key, this.returnData[key]);
+            }
+        });
     }
 
     // Web Socket 데이터 처리
     handleRemoteData(handler) {
-        this.readablePorts = handler.read('readablePorts');
-        var digitalValue = this.remoteDigitalValue;
-        for (var port = 0; port < 14; port++) {
-            digitalValue[port] = handler.read(port);
+        Object.keys(this.PORT_MAP).forEach((port) => {
+            this.PORT_MAP[port] = handler.read(port);
+        });
+        Object.keys(this.SENSOR_MAP).forEach((port) => {
+            this.SENSOR_MAP[port] = handler.read(port);
+        });
+
+        const receivedStatusColor = this.STATUS_COLOR_MAP[
+            handler.read('STATUS_COLOR')
+        ];
+        if (
+            receivedStatusColor !== undefined &&
+            this.CURRENT_STATUS_COLOR.COLOR !== receivedStatusColor
+        ) {
+            this.CURRENT_STATUS_COLOR = {
+                COLOR: receivedStatusColor,
+                APPLIED: false,
+            };
         }
     }
 
     // 하드웨어에 전달할 데이터
     requestLocalData() {
-        var queryString = [];
+        let isSendData = false;
+        const initBuf = this.makeInitBuffer([0x80], [0, 0]);
+        let sendBody;
+        this.sensorCheck();
+        let skipPortOutput = false;
 
-        var readablePorts = this.readablePorts;
-        if (readablePorts) {
-            for (var i in readablePorts) {
-                var query = (5 << 5) + (readablePorts[i] << 1);
-                queryString.push(query);
+        //이전 포트결과에서 변한부분이 있는지 확인
+        if (this.LAST_PORT_MAP) {
+            const arr = Object.keys(this.PORT_MAP).filter((port) => {
+                const map1 = this.PORT_MAP[port];
+                const map2 = this.LAST_PORT_MAP[port];
+                return !(map1.type === map2.type && map1.power === map2.power);
+            });
+            skipPortOutput = arr.length === 0;
+        }
+
+        //변한부분이 있다면 포트에 보낼 데이터를 생성
+        if (!skipPortOutput) {
+            isSendData = true;
+            this.LAST_PORT_MAP = _.cloneDeep(this.PORT_MAP);
+            sendBody = this.makePortCommandBuffer(isSendData);
+        }
+
+        //상판 LED 컬러 변경 요청이 있는 경우 변경 커맨드를 페이로드에 추가
+        if (this.CURRENT_STATUS_COLOR.APPLIED === false) {
+            isSendData = true;
+            const statusLedCommand = this.makeStatusColorCommandBuffer(
+                sendBody
+            );
+
+            if (!sendBody) {
+                sendBody = statusLedCommand;
+            } else {
+                sendBody = Buffer.concat([sendBody, statusLedCommand]);
             }
         }
-        var readablePortsValues =
-            (readablePorts && Object.values(readablePorts)) || [];
-        var digitalValue = this.remoteDigitalValue;
-        for (var port = 0; port < 14; port++) {
-            if (readablePortsValues.indexOf(port) > -1) {
-                continue;
-            }
-            var value = digitalValue[port];
-            if (value === 255 || value === 0) {
-                var query = (7 << 5) + (port << 1) + (value == 255 ? 1 : 0);
-                queryString.push(query);
-            } else if (value > 0 && value < 255) {
-                var query = (6 << 5) + (port << 1) + (value >> 7);
-                queryString.push(query);
-                query = value & 127;
-                queryString.push(query);
-            }
+
+        if (isSendData && sendBody) {
+            const totalLength = initBuf.length + sendBody.length;
+            const sendBuffer = Buffer.concat([initBuf, sendBody], totalLength);
+            this.checkByteSize(sendBuffer);
+            return sendBuffer;
         }
-        return queryString;
+
+        return null;
     }
 
+    makeStatusColorCommandBuffer() {
+        this.CURRENT_STATUS_COLOR.APPLIED = true;
+        const statusLedCommand = new Buffer([
+            0x82,
+            0x1b,
+            this.CURRENT_STATUS_COLOR.COLOR.key,
+        ]);
+
+        return new Buffer(statusLedCommand);
+    }
+
+    makePortCommandBuffer() {
+        let sendBody = null;
+        Object.keys(this.PORT_MAP).forEach((port) => {
+            let backBuffer;
+            let frontBuffer;
+            const portMap = this.PORT_MAP[port];
+            let brake = 0;
+            let checkPortMap = this.CHECK_PORT_MAP[port];
+            if (!checkPortMap || portMap.id !== checkPortMap.id) {
+                let portOut;
+                let power = Number(portMap.power);
+                if (portMap.type === this.motorMovementTypes.Power) {
+                    const time = Number(portMap.time) || 0;
+                    brake = 0;
+                    if (power > 100) {
+                        power = 100;
+                    } else if (power < -100) {
+                        power = -100;
+                    } else if (power === 0) {
+                        brake = 1;
+                    }
+
+                    if (time <= 0) {
+                        // infinity output port mode
+                        portOut = new Buffer([
+                            0xa4,
+                            0x81,
+                            0,
+                            0x81,
+                            this.outputPort[port],
+                            0x81,
+                            power,
+                            0xa6,
+                            0x81,
+                            0,
+                            0x81,
+                            this.outputPort[port],
+                        ]);
+                    } else {
+                        // time set mode 232, 3 === 1000ms
+                        frontBuffer = new Buffer([
+                            0xad,
+                            0x81,
+                            0,
+                            0x81,
+                            this.outputPort[port],
+                            0x81,
+                            power,
+                            0x83,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0x83,
+                        ]);
+                        backBuffer = new Buffer([
+                            0x83,
+                            0,
+                            0,
+                            0,
+                            0,
+                            0x81,
+                            brake,
+                        ]);
+                        const timeBuffer = new Buffer(4);
+                        timeBuffer.writeInt32LE(time);
+                        portOut = Buffer.concat([
+                            frontBuffer,
+                            timeBuffer,
+                            backBuffer,
+                        ]);
+                    }
+                } else {
+                    const degree = Number(portMap.degree) || 0;
+                    frontBuffer = new Buffer([
+                        0xac,
+                        0x81,
+                        0,
+                        0x81,
+                        this.outputPort[port],
+                        0x81,
+                        power,
+                        0x83,
+                        0,
+                        0,
+                        0,
+                        0,
+                        0x83,
+                    ]);
+                    backBuffer = new Buffer([0x83, 0, 0, 0, 0, 0x81, brake]);
+                    const degreeBuffer = new Buffer(4);
+                    degreeBuffer.writeInt32LE(degree);
+                    portOut = Buffer.concat([
+                        frontBuffer,
+                        degreeBuffer,
+                        backBuffer,
+                    ]);
+                }
+
+                if (portOut) {
+                    if (!sendBody) {
+                        sendBody = new Buffer(portOut);
+                    } else {
+                        sendBody = Buffer.concat([sendBody, portOut]);
+                    }
+                }
+
+                this.CHECK_PORT_MAP[port] = this.PORT_MAP[port];
+            }
+        });
+        return sendBody;
+    }
+
+    /**
+     * requestInitialData(external interval) -> sensorChecking(interval) -> sensorCheck
+     * 센서데이터를 연결해 한번에 보낸다.
+     * output 이 존재하는 Port 1,2,3,4 번을 체크한다.
+     *
+     * 보내는 데이터는 여러개의 데이터 명령이고 받는 결과 또한 여러개의 결과값이다.
+     */
+    sensorCheck() {
+        if (!this.isSensing) {
+            this.isSensing = true;
+            const initBuf = this.makeInitBuffer(
+                [0],
+                [this.wholeResponseSize, 0]
+            );
+            const counter = initBuf.readInt16LE(2); // initBuf의 index(2) 부터 2byte 는 counter 에 해당
+            this.SENSOR_COUNTER_LIST[counter] = true;
+            let sensorBody = [];
+            let index = 0;
+            Object.keys(this.SENSOR_MAP).forEach((p) => {
+                let mode = 0;
+                if (this.returnData[p] && this.returnData[p]['type']) {
+                    mode = this.SENSOR_MAP[p]['mode'] || 0;
+                }
+                const port = Number(p) - 1;
+                index = port * this.commandResponseSize;
+                const modeSet = new Buffer([
+                    0x99,
+                    0x05,
+                    0,
+                    port,
+                    0xe1,
+                    index,
+                    0xe1,
+                    index + 1,
+                ]);
+                const readySi = new Buffer([
+                    0x99,
+                    0x1d,
+                    0,
+                    port,
+                    0,
+                    mode,
+                    1,
+                    0xe1,
+                    index + 2,
+                ]);
+
+                if (!sensorBody.length) {
+                    sensorBody = Buffer.concat([modeSet, readySi]);
+                } else {
+                    sensorBody = Buffer.concat([sensorBody, modeSet, readySi]);
+                }
+            });
+            /*
+			리팩토링 없는 isButtonPressed 시작
+			sensorBody
+			* */
+            let offsetAfterPortResponse = 4 * this.commandResponseSize; // 포트는 [0~3] 까지다.
+            Object.keys(this.BUTTON_MAP).forEach((button) => {
+                const buttonPressedCommand = new Buffer([
+                    0x83, // opUI_BUTTON
+                    0x09, // pressed
+                    this.BUTTON_MAP[button].key,
+                    0xe1,
+                    offsetAfterPortResponse++,
+                ]);
+
+                sensorBody = Buffer.concat([sensorBody, buttonPressedCommand]);
+            });
+
+            /*
+            리팩토링 없는 isButtonPressed 종료
+             */
+            const totalLength = initBuf.length + sensorBody.length;
+            const sendBuffer = Buffer.concat(
+                [initBuf, sensorBody],
+                totalLength
+            );
+            this.checkByteSize(sendBuffer);
+            this.sp.write(sendBuffer);
+        }
+    }
 
     connect() { }
 
@@ -177,4 +485,4 @@ class artik053 extends BaseModule {
     reset() { }
 }
 
-module.exports = new artik053();
+module.exports = new ev3();
